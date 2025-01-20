2324ed331cece2d29feb93fb82337730e1845fa8
asthapatel1125 <asthapatel.1125@gmail.com>
2025-01-19 19:05:28 -0500

    added middleware and configured main.py to automatically run the uvicorn server

--- Customer_Authentication/src/app/main.py
+++ Customer_Authentication/src/app/main.py
@@ -1,11 +1,23 @@
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from routers.auth_router import router as auth_router
 import uvicorn, os
 from fastapi import Request
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Middleware definition
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*", "http://localhost:8000", "http://localhost:8000"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=[""],
+)
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 static_dir = os.path.join(base_dir, "views", "static")
@@ -21,5 +33,11 @@ app.include_router(auth_router, prefix="/auth")
 async def root(request : Request):
      return templates.TemplateResponse("login.html", {"request": request})
 
+load_dotenv(dotenv_path='./app/config/.env')
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(
+        app="main:app",
+        reload=True if os.environ.get("ENVIRONMENT") == "dev" else False,
+        workers=1,
+    )
