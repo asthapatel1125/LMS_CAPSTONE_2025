ec01c37c233784d928296fde6d13accd89f9d07c
asthapatel1125 <asthapatel.1125@gmail.com>
2025-03-08 16:16:08 -0500

    created api endpoints for Search database module

--- Search/src/app/models/books.py
+++ Search/src/app/models/books.py
@@ -47,3 +47,80 @@ def list_books():
     db = get_db()
     books = list(db["books"].find())
     return [Book(**book) for book in books]
+
+
+'''Search Bar Queries'''
+# Get a list of books by title
+@app.get("/books/{title}", response_model=List[Book])
+def get_books_by_rating(title: str):
+    
+    return "Book List by title"
+
+
+# Get a list of books by isbn (Already done above)
+
+
+# Get a list of books by author
+@app.get("/books/{author}", response_model=List[Book])
+def get_books_by_rating(author: str):
+    
+    return "Book List by author"
+
+
+# Get a list of books by publisher
+@app.get("/books/{publisher}", response_model=List[Book])
+def get_books_by_rating(publisher: str):
+    
+    return "Book List by publisher"
+
+
+
+'''Filter Queries (Some can be utilized for search bar queires too)'''
+
+# Get a list of books with ratings higher than or equal to given rating.
+@app.get("/books/{rating}", response_model=List[Book])
+def get_books_by_rating(rating: float):
+    
+    return "Popular Book List"
+
+
+'''not finished'''
+# Get Newest books (NOT FINISHED)
+@app.get("/books/{release_date}", response_model=List[Book])
+def get_latest_releases(release_date: str):
+    
+    return "Newely Book List"
+
+
+# Get Avialable books
+@app.get("/books/{status}", response_model=List[Book])
+def get_books_by_status(status: str):
+    
+    return "Available Book List"
+
+
+# Get specifically formatted items
+@app.get("/books/{format}", response_model=List[Book])
+def get_books_by_format(format: str):
+    
+    return "Specifically formatted Book List"
+
+
+# Get a list of items based on a given genre
+@app.get("/books/{genre}", response_model=List[Book])
+def get_books_by_genre(genre: str):
+    
+    return "Specific genre Book List"
+
+# Get a list of items based on the type of audience chosen
+@app.get("/books/{kidFriendly}", response_model=List[Book])
+def get_books_by_audience(kidFriendly: bool):
+    
+    return "Specific audience Book List"
+
+
+# Get Book image
+@app.get("/book_images/{_id}", response_model=BookImages)
+def get_book_images(_id: str):
+    
+    return "book image object"
